878fa2913fcc59f6b2591a2ffe568ec6f89edac0
RL20 <harel_shalom@yahoo.com>
2021-12-03 22:48:26 +0200

    22:48

--- snake.js
+++ snake.js
@@ -38,43 +38,32 @@ for (let i = 1; i <= 20; i++) {
     grid.push({ x: i, y: j, blockClass: blocksToClass[mtrx[j - 1]], elementHtml: "" });
   }
 }
-
-// const snakeBody = [
-//   { x: 10, y: 11 },
-//   { x: 11, y: 11 },
-//   { x: 12, y: 11 },
-//   { x: 13, y: 11 },
-//   { x: 14, y: 11 },
-// ];
-export function update() {
-  // for (let i = snakeBody.length - 2; i >= 0; i--) {
-  //   snakeBody[i + 1] = { ...snakeBody[i] };
-  // }
-  // snakeBody[0].x += 0;
-  // snakeBody[0].y += 1;
-}
-// export function draw(gameBoard) {
-//   gameBoard.innerHTML = "";
-//   grid.forEach((segment) => {
-//     const snakeElement = document.createElement("div");
-//     snakeElement.style.gridRowStart = segment.y;
-//     snakeElement.style.gridColumnStart = segment.x;
-//     snakeElement.classList.add("sky");
-//     gameBoard.append(snakeElement);
-//   });
-// }
+let lastBlockStorage = "";
+let divStorLastBlock = document.querySelector("#stor-last-block");
+export function update() {}
 
 export function draw(gameBoard) {
   gameBoard.innerHTML = "";
   grid.forEach((row) => {
     // gameBoard.innerHTML = "";
-    console.log(row);
+    // console.log(row);
     // console.log(row.blockClass);
     const blockElement = document.createElement("div");
+
+    // row.elementHtml = blockElement;
     blockElement.classList.add(row.blockClass);
+    blockElement.addEventListener("click", (e) => {
+      lastBlockStorage = e.target.className;
+      divStorLastBlock.classList.add(lastBlockStorage);
+      e.target.className = "";
+      e.target.classList.add("sky");
+
+      // e.target.style.backgroundColor = "pink";
+    });
     gameBoard.append(blockElement);
   });
 }
+
 // export function draw(gameBoard) {
 //   gameBoard.innerHTML = "";
 //   grid.forEach((segment) => {
