28f7eff0b1eb1142b2c1d8a47512d9677445642c
RL20 <harel_shalom@yahoo.com>
2021-12-04 16:40:26 +0200

    16:40

--- game.js
+++ game.js
@@ -1,26 +1,124 @@
-import { update as updateSnake, draw as drawSnake, axe as a, pickaxe as p, shovel as s } from "./snake.js";
-let lastRenderTime = 0;
 const gameBoard = document.querySelector("#game-board");
+const blocksToClass = {
+  1: "transparent",
+  0: "sky",
+  2: "dirt",
+  3: "gress",
+  4: "rock",
+  5: "trunk",
+  6: "leaves",
+  7: "clouds",
+};
 
-function main(currenTime) {
-  // window.requestAnimationFrame(main);
-  // const secondSinceLastRender = (currenTime - lastRenderTime) / 1000;
-  // if (secondSinceLastRender < 1 / SANKE_SPEED) return;
-  // lastRenderTime = currenTime;
-  update();
-  draw();
+const matrix = [
+  [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 7, 7, 7, 7, 0, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
+  [0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
+  [0, 0, 0, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 5, 0, 0, 4],
+  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+];
+
+let grid = [];
+for (let i = 1; i <= 20; i++) {
+  let mtrx = matrix[i - 1];
+
+  for (let j = 1; j <= 20; j++) {
+    grid.push({ x: i, y: j, blockClass: blocksToClass[mtrx[j - 1]], elementHtml: "" });
+  }
 }
-window.requestAnimationFrame(main);
-function update() {
-  updateSnake();
+var build = false; //the stat when click on block if true click on block ass class
+var classToPush = ""; //block kept befor  build somting new
+let lastBlockStorage = "";
+let divStorLastBlock = document.querySelector("#stor-last-block");
+
+let axe = false;
+let pickaxe = false;
+let shovel = false;
+
+// event listner
+divStorLastBlock.addEventListener("click", (e) => {
+  build = true;
+  classToPush = e.target.className;
+});
+
+function draw(gameBoard) {
+  gameBoard.innerHTML = "";
+  grid.forEach((row) => {
+    const blockElement = document.createElement("div");
+
+    blockElement.classList.add(row.blockClass); //draw the start from matrix
+    blockElement.addEventListener("click", (e) => {
+      let curBlockClass = e.target.className;
+      if (!build) {
+        if (axe) {
+          if (curBlockClass === "leaves" || curBlockClass === "trunk") {
+            lastBlockStorage = curBlockClass;
+            divStorLastBlock.className = "";
+            divStorLastBlock.classList.add(lastBlockStorage);
+            e.target.className = "";
+            e.target.classList.add("sky");
+          } else if (curBlockClass === "rock" || curBlockClass === "dirt" || curBlockClass === "gress") {
+            axeBtn.classList.add("tools-fram-clicked-wrong");
+            setTimeout((e) => {
+              axeBtn.classList.remove("tools-fram-clicked-wrong");
+            }, 100);
+          } else return;
+        }
+        if (pickaxe) {
+          if (curBlockClass === "rock") {
+            lastBlockStorage = curBlockClass;
+            divStorLastBlock.className = "";
+            divStorLastBlock.classList.add(lastBlockStorage);
+            e.target.className = "";
+            e.target.classList.add("sky");
+          } else if (curBlockClass === "leaves" || curBlockClass === "trunk" || curBlockClass === "dirt" || curBlockClass === "gress") {
+            PickaxeBtn.classList.add("tools-fram-clicked-wrong");
+            setTimeout((e) => {
+              PickaxeBtn.classList.remove("tools-fram-clicked-wrong");
+            }, 100);
+          } else return;
+        }
+        if (shovel) {
+          if (curBlockClass === "dirt" || curBlockClass === "gress") {
+            lastBlockStorage = curBlockClass;
+            divStorLastBlock.className = "";
+            divStorLastBlock.classList.add(lastBlockStorage);
+            e.target.className = "";
+            e.target.classList.add("sky");
+          } else if (curBlockClass === "leaves" || curBlockClass === "trunk" || curBlockClass === "rock") {
+            ShovelBtn.classList.add("tools-fram-clicked-wrong");
+            setTimeout((e) => {
+              ShovelBtn.classList.remove("tools-fram-clicked-wrong");
+            }, 100);
+          } else return;
+        }
+      } else {
+        e.target.className = "";
+        e.target.classList.add(lastBlockStorage);
+        build = false;
+      }
+    });
+    gameBoard.append(blockElement);
+  });
 }
-function draw() {
-  drawSnake(gameBoard);
-}
-let axe = a;
-let pickaxe = p;
-let shovel = s;
-//code for tools
+draw(gameBoard);
+
+//code for tools buttons
 let btns = document.querySelectorAll(".tools-fram");
 let PickaxeBtn = btns[0];
 let axeBtn = btns[1];
@@ -31,6 +129,8 @@ PickaxeBtn.addEventListener("click", (e) => {
   isContainsRemove(ShovelBtn, "tools-fram-clicked");
   PickaxeBtn.classList.toggle("tools-fram-clicked");
   pickaxe = !pickaxe;
+  axe = false;
+  shovel = false;
 });
 
 axeBtn.addEventListener("click", (e) => {
@@ -38,13 +138,17 @@ axeBtn.addEventListener("click", (e) => {
   isContainsRemove(ShovelBtn, "tools-fram-clicked");
   axeBtn.classList.toggle("tools-fram-clicked");
   axe = !axe;
-  console.log(axe);
+  pickaxe = false;
+  shovel = false;
+  // console.log(axe);
 });
 ShovelBtn.addEventListener("click", (e) => {
   isContainsRemove(axeBtn, "tools-fram-clicked");
   isContainsRemove(PickaxeBtn, "tools-fram-clicked");
   ShovelBtn.classList.toggle("tools-fram-clicked");
   shovel = !shovel;
+  axe = false;
+  pickaxe = false;
 });
 function isContainsRemove(btn, cssClass) {
   if (btn.classList.contains(cssClass)) btn.classList.toggle(cssClass);
--- scripts/tools.js
+++
@@ -1,8 +0,0 @@
-// a. Axe - for cutting trees
-// b. Pickaxe - for mining rocks
-// c. Shovel - for digging dirt
-let blocklastBlockStore=
-let axe;
-let Pickaxe;
-let Shovel;
-if block is wood or leaf  
