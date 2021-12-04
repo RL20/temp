cecb4e67dc956b8e94cc10227c7d9a40fc4f9811
RL20 <harel_shalom@yahoo.com>
2021-12-04 13:44:41 +0200

    13:44

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-import { update as updateSnake, draw as drawSnake, axe as a, pickaxe as p, shovel as s } from "./snake.js";
+// import { update as updateSnake, draw as drawSnake, axe, pickaxe, shovel } from "./snake.js";
+// import { update as updateSnake, draw as drawSnake, axe as a, pickaxe as p, shovel as s } from "./snake.js";
+import * as tools from "./snake.js";
 let lastRenderTime = 0;
 const gameBoard = document.querySelector("#game-board");
 
@@ -12,14 +14,17 @@ function main(currenTime) {
 }
 window.requestAnimationFrame(main);
 function update() {
-  updateSnake();
+  // updateSnake();
 }
 function draw() {
-  drawSnake(gameBoard);
+  // drawSnake(gameBoard);
 }
-let axe = a;
-let pickaxe = p;
-let shovel = s;
+// console.log("tools", axe, pickaxe, shovel);
+// a = "it works";
+
+// let axe = a;
+// let pickaxe = p;
+// let shovel = s;
 //code for tools
 let btns = document.querySelectorAll(".tools-fram");
 let PickaxeBtn = btns[0];
@@ -30,7 +35,8 @@ PickaxeBtn.addEventListener("click", (e) => {
   isContainsRemove(axeBtn, "tools-fram-clicked");
   isContainsRemove(ShovelBtn, "tools-fram-clicked");
   PickaxeBtn.classList.toggle("tools-fram-clicked");
-  pickaxe = !pickaxe;
+  tools.pickaxe = !tools.pickaxe;
+  console.log("pickaxe", pickaxe);
 });
 
 axeBtn.addEventListener("click", (e) => {
